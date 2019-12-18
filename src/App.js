a683d64e0d372f67ae8462a1be0ad56866270175
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-12-17 21:04:35 -0500

    quote bubble slides top-right

--- src/App.js
+++ src/App.js
@@ -4,17 +4,42 @@ import Typing from 'react-typing-animation';
 
 class App extends React.Component {
 
+  state = {
+    sliding: false,
+    top: 30,
+    left: 63
+  }
+
+ componentDidMount(){
+   setInterval(() => {
+      if(this.state.top > 15 && this.state.left < 70) {
+        this.setState({
+          sliding: true, 
+          top: this.state.top - 5,
+          left: this.state.left + 2
+        })
+      }
+   }, 2500)
+ }
+
   render(){
-   
+  
+  console.log(this.state)
     return (
+
       <div className="App">
+       <div className="notebook">
         <Typing>
-          <div className="speech-bubble">
+         { this.state.sliding ? <div className="speech-bubble"  style={{top: `${this.state.top}em`, left: `${this.state.left}%`}}> 
           <h2>Hello there!</h2>
           <p>My name is Derya!</p>
-          </div>
+          </div> : <div className="speech-bubble "> 
+          <h2>Hello there!</h2>
+          <p>My name is Derya!</p>
+          </div>}
           
         </Typing>
+        </div>
       </div>
     );
   }
