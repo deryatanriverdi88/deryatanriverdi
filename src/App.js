dc5617c0b6a66eeb7b82157dd3440826621d7219
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-12-18 14:18:20 -0500

    post-it's added

--- src/App.js
+++ src/App.js
@@ -5,24 +5,8 @@ import Typing from 'react-typing-animation';
 class App extends React.Component {
 
   state = {
-    sliding: true,
-    top: 20,
-    left: 20, 
-    quote: false
+    
   }
-
- componentDidMount(){
-   setInterval(() => {
-      if(this.state.top > 8 && this.state.left < 70) {
-        this.setState({
-          
-          top: this.state.top - 10,
-          left: this.state.left + 15,
-          sliding: false
-        })
-      }
-   },2500)
- }
 
  
 
@@ -33,24 +17,23 @@ class App extends React.Component {
 
       <div className="App">
       <div className="notebook">
-       { this.state.sliding ? <Typing>
+        <Typing>
        
-         <div className="speech-bubble"  style={{top: `${this.state.top}em`, left: `${this.state.left}%`}}> 
+         <div className="speech-bubble" > 
           <h2>Welcome !</h2>
           <p>My name is Derya!</p>
           
           </div>
-        </Typing> : 
-         <div className="speech-bubble"  style={{top: `${this.state.top}em`, left: `${this.state.left}%`}}> 
-         <h2>Welcome !</h2>
-         <p>My name is Derya!</p>
-         
-         </div>}
-        <Typing>
+        </Typing> 
+      
+
           <div className="text-div">
-           {this.state.quote ? <p className="p"> Let's dgfgsdfyfsygfysgfygsyfgehfgysgf Let's dgfgsdfyfsygfysgfygsyfgehfgysgf Let's dgfgsdfyfsygfysgfygsyfgehfgysgf Let's dgfgsdfyfsygfysgfygsyfgehfgysgf vvvvLet's dgfgsdfyfsygfysgfygsyfgehfgysgf  </p> :  null }
+            <div className="post-it-1"></div>
+            <div className="post-it-2"></div>
+            <div className="post-it-1"></div>
+            <div className="post-it-2"></div>
           </div>
-        </Typing>
+       
         </div>
         </div>
     );
