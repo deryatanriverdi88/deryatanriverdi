c5d50a5c8cead236a5c2d641734f6f22c9ab907a
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2020-08-25 04:22:37 -0400

    adds console.log

--- src/App.js
+++ src/App.js
@@ -109,9 +109,7 @@ class App extends React.Component {
   }
 
   render(){
- 
-  
-//  console.log(this.state)
+ console.log(this.state.windowSize)
     return (
       <div className="app">
          <div className="border">
